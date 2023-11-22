1a080b0d9f08366d8febd9e6d8bf8c4d4bd12366
Stanislaus Eichstaedt <stanislaus.eichstaedt@ch.sauter-bc.com>
2023-11-22 18:02:30 +0100

    Implement first draft retry logic

---
+++ VersionsFeedService/VersionParser/Transient/Retry.cs
@@ -0,0 +1,25 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace DotNETVersionFeed.Transient
+{
+    public class Retry<T> where T : Exception
+    {
+        public int RetryCount { get; set; }
+
+        public TimeSpan SleepDuration { get; set; } = TimeSpan.FromMilliseconds(200);
+
+        public AsyncRetryPolicy Policy { get; }
+
+        public T? ExceptionToCatch { get; } = new Exception() as T;
+
+        public Retry(int retryCount = 3)
+        {
+            RetryCount = retryCount;
+
+            Policy = Polly.Policy
+                .Handle<T>()
+                .WaitAndRetryAsync(RetryCount, sleepDurationProvider: _ => SleepDuration);
+        }
+    }
+}
