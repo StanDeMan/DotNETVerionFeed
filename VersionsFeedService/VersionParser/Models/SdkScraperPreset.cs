5611ac56afcc134cd1a44ef86f7bdc38b60d222c
Stanislaus Eichstaedt <stanislaus.eichstaedt@ch.sauter-bc.com>
2023-12-14 14:12:31 +0100

    Ass the preset class

---
+++ VersionsFeedService/VersionParser/Models/SdkScraperPreset.cs
@@ -0,0 +1,11 @@
+﻿using Version = VersionsFeedService.VersionParser.Architecture.Version;
+
+namespace VersionsFeedService.VersionParser.Models
+{
+    public class SdkScraperPreset
+    {
+        public Version Version { get; set; }
+
+        public required SdkArchitecture[] Families { get; set; }
+    }
+}
